36ce22cb2d9d8929b5d3ca2c637ecc5a78c03b3f
Frank Hua <frankhua21@gmail.com>
2020-10-06 17:27:22 -0400

    add switch page icon for main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { ArrowRightSquare, ArrowLeftSquare } from "react-bootstrap-icons";
 
 export default function Main(props) {
-  return <div>Page Main</div>;
+  return (
+    <div>
+      Page Main
+      <ArrowLeftSquare className="main-arrow-button main-arrow-left" onClick={() => props.setCurrPage("Left")} />
+      <ArrowRightSquare className="main-arrow-button main-arrow-right" onClick={() => props.setCurrPage("Right")} />
+    </div>
+  );
 }
