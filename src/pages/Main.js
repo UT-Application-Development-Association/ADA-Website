b1cf7cb9927314467363343f421e08ec44ab06af
shitang <tiffanyshi.tang@mail.utoronto.ca>
2020-11-20 21:46:18 -0500

    Added footnote (connect with us)

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 import { ArrowRightSquare, ArrowLeftSquare } from "react-bootstrap-icons";
 import Heading from "../components/main_page/Heading";
 import GongZhongHao from "../components/main_page/GongZhongHao.js";
+import Footnote from "../components/main_page/Footnote";
 
 export default function Main(props) {
   return (
@@ -31,6 +32,11 @@ export default function Main(props) {
         <div></div>
       </section>
       <section className="timeline-section"></section>
+      <section className='footnote'>
+        <div>
+          <Footnote />
+        </div>
+      </section>
     </div>
   );
 }
