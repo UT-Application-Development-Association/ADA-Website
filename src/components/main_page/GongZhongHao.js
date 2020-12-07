05719ea85047700ce9616e765553a7ff57ec8a4a
wuxc0116 <wu.xiangcheng@hotmail.com>
2020-12-07 09:55:54 +0800

    cherry-picked gongzhonghao change
    from commit 0eb5e819f14f7c3bd3aca4102a845064f83c14f8

--- src/components/main_page/GongZhongHao.js
+++ src/components/main_page/GongZhongHao.js
@@ -1,8 +1,46 @@
 import React from "react";
+//import "../../styles/main_page/_GongZhongHao.scss";
+//import "../../styles/styles/style.css.map";
 
 export default function GongZhongHao(props) {
     return (
-        <div className="gzh-container">
+        // <div className="gzh-container">
+        // </div>
+        <div className="MainGrid">
+            <article>
+                <a href="https://mp.weixin.qq.com/s?src=11&timestamp=1607286461&ver=2750&signature=jI8d4NQ08ctBdDO7eVHA0QKKAl6dhtO8Ip3QSr0PqnjQeG-YkH0PhDAx6sK00v5eAXBEv7PC8jdH0jZ0TW8g4wTCigfITtG0jxQuUfMynI*z2L47C1lXF8GcTGrsp9SR&new=1" class="button">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/East_West_sushi_01.jpg/520px-East_West_sushi_01.jpg" alt="Sample photo"></img>
+                </a>
+                <div className="MainText">
+                    <br></br>
+                    <h5>多伦多日料外卖大赏</h5>
+                    <p>什么？在家也能吃到新鲜的日料？</p>
+                    
+                </div>
+            </article>
+            <article>
+            <a href="https://mp.weixin.qq.com/s?src=11&timestamp=1607298911&ver=2751&signature=ObJhORkkKeqABnyU7yhfMITKKqLSnh*ETWTtNHxOftJzjraTPRhmcRPBDbjUZoBd*swG4V4pGlW0bLEaq8cE*i19ntou1JotXwAJMYV6HqGLBSwDUGP6kPxn-lIwXSAK&new=1">
+                <img src="https://www.pearsoneducation.ca/wp-content/uploads/2019/09/shutterstock_116618521.jpg" alt="Sample photo"></img>
+            </a>
+                <div className="MainText">
+                    <br></br>
+                    <h5>疫情下的多伦多大学</h5>
+                    <p>你是否开始怀念多大的校园了呢？</p>
+                    
+                </div>
+            </article>
+            <article>
+            <a href="https://mp.weixin.qq.com/s?src=11&timestamp=1607297301&ver=2751&signature=ObJhORkkKeqABnyU7yhfMITKKqLSnh*ETWTtNHxOftKLwzwAWIeCNXZ2DP30K36327BjRPYHS8mp87aAa2xsudXcUjJGD276y50o*ViW3KftKKizsUInzezUtIXjX09L&new=1">
+                <img src="https://ciracollege.com/wp-content/uploads/2020/11/How-to-Learn-Python.jpg" alt="Sample photo"></img>        
+            </a>
+                <div className="MainText">
+                    <br></br>
+                    <h5> 多大CS为何从Python开始</h5>
+                    <p>编程语言千千万，不知学啥怎么办？</p>
+                    
+                </div>
+            </article>
+
         </div>
     );
 }
