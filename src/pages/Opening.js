36e629cccd1338313a37e34ec03c03e759060b9d
Keying Chen <35384984+KKyInGG@users.noreply.github.com>
2021-01-24 01:05:48 -0500

    kk's main page

--- src/pages/Opening.js
+++ src/pages/Opening.js
@@ -2,8 +2,9 @@ import React from "react";
 
 export default function Opening(props) {
   return (
-    <div>
-      Page Opening
+    <div class="Container">
+      <img class="BackgroundImage" src={require('../imgs/5s.gif')}></img>
+      <img class="Logo" src={require('../imgs/MainLogo.png')}></img>
       <button className="opening-to-main-button" onClick={() => props.setCurrPage("Main")}>To main</button>
     </div>
   );
