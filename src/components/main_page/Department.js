f493c27cc95123f6e540216e64537b76dea51813
themarkyone <markyin2001@gmail.com>
2021-01-30 22:30:05 -0500

    update images for department section

--- src/components/main_page/Department.js
+++ src/components/main_page/Department.js
@@ -1,4 +1,8 @@
 import React from "react";
+import tech from "../../assets/img/dept_tech.png";
+import pr from "../../assets/img/dept_pr.png";
+import mkt from "../../assets/img/dept_mkt.png";
+import event from "../../assets/img/dept_event.png";
 
 export default function Department(props) {
     return (
@@ -6,6 +10,24 @@ export default function Department(props) {
             <h1>This is Our Departments</h1>
             <div className="departments-title">
                 <h1>Departments</h1>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm">
+                        <img src={ tech } alt=""></img>
+                    </div>
+                    <div className="col-sm">
+                        <img src={ pr } alt=""></img>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm">
+                        <img src={ mkt } alt=""></img>
+                    </div>
+                    <div className="col-sm">
+                        <img src={ event } alt=""></img>
+                    </div>
+                </div>
             </div>
         </div>
     );
