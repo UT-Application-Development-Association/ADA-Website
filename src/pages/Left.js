bfa6d87ef4b93f6f150e9df6c3cfde43b71c6bc5
rainyuxuan <yu2001xuan@outlook.com>
2021-05-01 00:15:46 -0400

    feat. [FullTimeline]: Added FullTimeline section;

--- src/pages/Left.js
+++ src/pages/Left.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { ArrowRightSquare } from "react-bootstrap-icons";
+import FullTimeline from "../components/left_page/FullTimeline.js";
 
 export default function Left(props) {
   return (
-    <div>
-      Page Left
+    <div className="left-page">
       {!props.isMobile && (
         <ArrowRightSquare
           className="left-arrow-button"
           onClick={() => props.setCurrPage("Main")}
         />
       )}
+
+      <section className="full-timeline-section">
+        <FullTimeline/>
+      </section>
     </div>
   );
 }
