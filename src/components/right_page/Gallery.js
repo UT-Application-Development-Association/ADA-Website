857592096e48d94de7b1a883a2eab77beb3b13ff
rainyuxuan <yu2001xuan@outlook.com>
2021-05-04 01:31:59 -0400

    style [Gallery]: changed some styles;

--- src/components/right_page/Gallery.js
+++ src/components/right_page/Gallery.js
@@ -2,23 +2,23 @@ import React from "react";
 import Heading from "../../components/main_page/Heading";
 import GalleryImg from "./GalleryImg";
 
-import deptEvent from "../../assets/img/dept_event.png";
-import deptMkt from "../../assets/img/dept_mkt.png";
-import deptPr from "../../assets/img/dept_pr.png";
-import deptTech from "../../assets/img/dept_tech.png";
-
 import newyear202001 from "../../assets/img/gallery/202001-newyear202001.jpg";
 import if202008 from "../../assets/img/gallery/202008-if202008.png";
 import yingxin202010v1 from "../../assets/img/gallery/202010-yingxin202010_1.png";
 import yingxin202010v2 from "../../assets/img/gallery/202010-yingxin202010_2.png";
+import clubcarnival2019 from "../../assets/img/events/ClubCarnival2019.jpeg";
+import yingxin2019 from "../../assets/img/events/Welcoming2019.jpeg";
+
 
 
 //padding and width for image should be adjusted when replacing image source
 export default function Gallery(props) {
     return (
         <div className="gallery-container container">
+        
             <div className="row">
-                <div className="col-12 col-md-8 col-lg-6">
+            
+                <div className="col-10 col-md-6 mx-auto">
                     <Heading text="我们是谁" />
                     <div className="img-row">
                         <GalleryImg src={newyear202001} text="2020新年晚会 2020-01"/>
@@ -26,11 +26,17 @@ export default function Gallery(props) {
                     <div className="img-row">
                         <GalleryImg src={if202008} text="首届Innovation Fair合影 2020-08"/>
                     </div>
+                    <div className="img-row">
+                        <GalleryImg src={clubcarnival2019} text="Club Carnival 2019-09"/>
+                    </div>
                 </div>
 
-                <div className="col-12 col-md-8 col-lg-6">
+                <div className="col-10 col-md-6 mx-auto">
                     <div className="img-row">
                         <GalleryImg src={yingxin202010v1} text="线上迎新 2020-10"/>
+                    </div>
+                    <div className="img-row">
+                        <GalleryImg src={yingxin2019} text="迎新会 2019-10"/>
                     </div>
                     <div className="img-row">
                         <GalleryImg src={yingxin202010v2} text="线上迎新 2020-10"/>
