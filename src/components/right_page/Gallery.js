db60b942a8e73be06b6934ec6cb3147baaf9b2be
rainyuxuan <yu2001xuan@outlook.com>
2021-05-04 00:51:56 -0400

    fix [Gallery]: rename images; added img component

--- src/components/right_page/Gallery.js
+++ src/components/right_page/Gallery.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Heading from "../../components/main_page/Heading";
+import GalleryImg from "./GalleryImg";
 
 import deptEvent from "../../assets/img/dept_event.png";
 import deptMkt from "../../assets/img/dept_mkt.png";
 import deptPr from "../../assets/img/dept_pr.png";
 import deptTech from "../../assets/img/dept_tech.png";
+
+import newyear202001 from "../../assets/img/gallery/202001-newyear202001.jpg";
+import if202008 from "../../assets/img/gallery/202008-if202008.png";
+import yingxin202010v1 from "../../assets/img/gallery/202010-yingxin202010_1.png";
+import yingxin202010v2 from "../../assets/img/gallery/202010-yingxin202010_2.png";
+
 
 //padding and width for image should be adjusted when replacing image source
 export default function Gallery(props) {
@@ -14,30 +21,19 @@ export default function Gallery(props) {
                 <div className="col-12 col-md-8 col-lg-6">
                     <Heading text="我们是谁" />
                     <div className="img-row">
-                        <img src={deptMkt} alt="Failed to load" className="" />
+                        <GalleryImg src={newyear202001} text="2020新年晚会 2020-01"/>
                     </div>
                     <div className="img-row">
-                        <img src={deptTech} alt="Failed to load"  className=""/>
-                    </div>
-                    <div className="img-row">
-                        <img src={deptMkt} alt="Failed to load" style={{ "width": "60%"}} />
-                        <img src={deptPr} alt="Failed to load" style={{ "width": "40%", "margin-left": "8px" }} />
+                        <GalleryImg src={if202008} text="首届Innovation Fair合影 2020-08"/>
                     </div>
                 </div>
 
                 <div className="col-12 col-md-8 col-lg-6">
                     <div className="img-row">
-                        <img src={deptTech} alt="Failed to load" style={{ "width": "100%" }} />
+                        <GalleryImg src={yingxin202010v1} text="线上迎新 2020-10"/>
                     </div>
                     <div className="img-row">
-                        <img src={deptEvent} alt="Failed to load" style={{ "width": "50%", "margin-right": "4px" }} />
-                        <img src={deptEvent} alt="Failed to load" style={{ "width": "50%", "margin-left": "4px" }} />
-                    </div>
-                    <div className="img-row">
-                        <img src={deptTech} alt="Failed to load" style={{ "width": "100%" }} />
-                    </div>
-                    <div className="img-row">
-                        <img src={deptTech} alt="Failed to load" style={{ "width": "100%" }} />
+                        <GalleryImg src={yingxin202010v2} text="线上迎新 2020-10"/>
                     </div>
                 </div>
             </div>
