32ab8bcb60c939586411a0bccd9816bbff54a5d4
rainyuxuan <yu2001xuan@outlook.com>
2021-05-11 03:34:43 -0400

    little style change

--- src/components/main_page/IntroSection.js
+++ src/components/main_page/IntroSection.js
@@ -6,13 +6,9 @@ export default function IntroSection(props) {
         <div className="intro-container container">
             <Heading text="基本介绍"/>
             <div className="intro-text paragraph my-3 my-md-4">
-                    {/* UTADA (University of Toronto Application Development Association)是加拿大多伦多大学学生创办的华人CS兴趣社团。<br></br>
-                    我们目前拥有约160名社团成员，一直致力于通过CS技术和社团旗下微信公众号UT助手为多大学生提供服务。<br></br>
-                    社团活动包含Workshop、Tutorial、Techtalk等。<br></br>
-                    其中TECH部门涵盖了IOS, Android, Web三大方向,让每一个对CS特定方向有热忱的小伙伴都能获得进步并找到自己的归属。<br></br> */}
                         我！们！是！University of Toronto Application Development Association（多伦多大学应用开发社），你也可以简称我们为UTADA。
                         <br/><br/>
-                        UTADA成立于2014年秋天。在这六年里，我们一直致力于创造一个属于多伦多大学的开放性技术社区。无论是走在路边突然的灵光一闪，还是经过深思熟虑的创意，我们都会倾尽全力帮你实现。无论是你CS萌新，还是技术大佬，总能在ADA找到适合自己的开发项目，积累宝贵的应用开发经验。
+                        UTADA成立于2014年秋天。在这七年里，我们一直致力于创造一个属于多伦多大学的开放性技术社区。无论是走在路边突然的灵光一闪，还是经过深思熟虑的创意，我们都会倾尽全力帮你实现。无论是你CS萌新，还是技术大佬，总能在ADA找到适合自己的开发项目，积累宝贵的应用开发经验。
                         <br/><br/>
                         除此之外，我们会定期举办干货Workshop，内容覆盖硬核编程技巧，课程选择，和职业发展。在往期活动中，我们邀请到了来自华为，IBM，安大略省政府，花旗银行，以及其他知名企业的嘉宾给社员们传授宝贵经验。
                         <br/><br/>
