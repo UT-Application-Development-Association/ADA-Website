da57f37d792294fc6b3cb2b2b15e570cc13a55fc
rainyuxuan <yu2001xuan@outlook.com>
2021-05-12 02:34:12 -0400

    fix [GZH]: fixed gzh styles;

--- src/components/main_page/GongZhongHao/GongZhongHao.js
+++ src/components/main_page/GongZhongHao/GongZhongHao.js
@@ -3,6 +3,7 @@ import React from "react";
 import Heading from "../Heading";
 import GongZhongHaoArticle from "./GongZhongHaoArticle";
 
+import qrImg from "../../../assets/img/qrcode.jpg";
 import img0 from "../../../assets/img/gongzhonghao/0.jpg";
 import img1 from "../../../assets/img/gongzhonghao/1.jpg";
 import img2 from "../../../assets/img/gongzhonghao/2.jpg";
@@ -15,6 +16,9 @@ export default function GongZhongHao(props) {
                 <GongZhongHaoArticle id={0} image={img0}/>
                 <GongZhongHaoArticle id={1} image={img1} />
                 <GongZhongHaoArticle id={2} image={img2}/>
+                <div className="QR-container">
+                    <img src={qrImg} alt="多大助手" />
+                </div>
             </div>
         </div>
     );
