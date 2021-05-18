8939cc5cdb677863b612fba7ebbb8bcbf0f74288
Falanke21 <frankhua21@gmail.com>
2021-05-18 01:45:55 -0400

    deploy build website of commit 352fa2d

---
+++ precache-manifest.dd9fb1da3d1d28f1ec8ff097ed2b5276.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a950c6318ccf41f826f6aed495f7462",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d4aeae2efe93b4477f6a",
+    "url": "./static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "135be142fe5802ed72b1",
+    "url": "./static/css/main.57070624.chunk.css"
+  },
+  {
+    "revision": "d4aeae2efe93b4477f6a",
+    "url": "./static/js/2.6706fe4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.6706fe4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "135be142fe5802ed72b1",
+    "url": "./static/js/main.4659f106.chunk.js"
+  },
+  {
+    "revision": "561228eb0e16f23a4d42",
+    "url": "./static/js/runtime-main.14e101d3.js"
+  },
+  {
+    "revision": "03141f8994028c9e84a0f98bf3217985",
+    "url": "./static/media/0.03141f89.jpg"
+  },
+  {
+    "revision": "1a2ee3fb90cf91b585eef72a3ce899fe",
+    "url": "./static/media/1.1a2ee3fb.jpg"
+  },
+  {
+    "revision": "a922fe6c69816a3fd3c995d0f175a96c",
+    "url": "./static/media/2.a922fe6c.jpg"
+  },
+  {
+    "revision": "9de0e1264bc86fa76c1d61db881e8e05",
+    "url": "./static/media/2017_white-logo-text.9de0e126.png"
+  },
+  {
+    "revision": "34fac88b3c040c13227d88497badaed5",
+    "url": "./static/media/2017_white-logo.34fac88b.png"
+  },
+  {
+    "revision": "bc50099c868c1b6286969708dbb8fe38",
+    "url": "./static/media/202001-newyear202001.bc50099c.jpg"
+  },
+  {
+    "revision": "e5b2370af62dbdf1b6b093a6b0d6b41b",
+    "url": "./static/media/202008-if202008.e5b2370a.png"
+  },
+  {
+    "revision": "b804eaf729ae9d9638e5f8085b9fb207",
+    "url": "./static/media/202010-yingxin202010_1.b804eaf7.png"
+  },
+  {
+    "revision": "e758f3bc71f5336cef00272b18f9fdce",
+    "url": "./static/media/202010-yingxin202010_2.e758f3bc.png"
+  },
+  {
+    "revision": "a78cdc027a2637c5107c39320b5e7082",
+    "url": "./static/media/ClubCarnival2019.a78cdc02.jpeg"
+  },
+  {
+    "revision": "4a3a1859717b0eb1623fdfb00de2ac77",
+    "url": "./static/media/CourseWiki.4a3a1859.png"
+  },
+  {
+    "revision": "a56bb81af2953d2caf72edc2b1c6ea38",
+    "url": "./static/media/EVENT.a56bb81a.png"
+  },
+  {
+    "revision": "d252f5d4503be0aba3a42605b1e2f134",
+    "url": "./static/media/InnovationFair2020.d252f5d4.png"
+  },
+  {
+    "revision": "9645dcbbbada5ff65eeb36a8525b393f",
+    "url": "./static/media/MKT.9645dcbb.png"
+  },
+  {
+    "revision": "693ee020d35a5195c9939108986f420a",
+    "url": "./static/media/OnlineClubFair2020.693ee020.jpeg"
+  },
+  {
+    "revision": "096aa1a6d9e10d8b34a974d08eebadbe",
+    "url": "./static/media/PR.096aa1a6.png"
+  },
+  {
+    "revision": "ec98e5f3ad056c35eabdf18612ce6f9d",
+    "url": "./static/media/TECH.ec98e5f3.png"
+  },
+  {
+    "revision": "488f8e9f273f805ff34f91d582c3e9de",
+    "url": "./static/media/UTHack2021.488f8e9f.png"
+  },
+  {
+    "revision": "b7da49a7162f2aa9674ec754b28e5dff",
+    "url": "./static/media/Welcoming2019.b7da49a7.jpeg"
+  },
+  {
+    "revision": "3088fd706ca9fc60b9680d163f5fd88a",
+    "url": "./static/media/qrcode.3088fd70.jpg"
+  }
+]);
